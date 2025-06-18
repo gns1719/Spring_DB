ef582cf450ae845f22b5aa92b94be23f69013adc
gns1719 <75101740+gns1719@users.noreply.github.com>
2025-06-18 23:20:34 +0900

    Spring DB study with inflearn

---
+++ src/main/java/hello/jdbc/domain/Member.java
@@ -0,0 +1,13 @@
+package hello.jdbc.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+    private String memberid;
+    private int money;
+}
